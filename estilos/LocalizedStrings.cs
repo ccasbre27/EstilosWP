d074ee0ee3f5d95b499ca2ce1bd0c05e5070850b
Carlos Castro Brenes <ccasbre27@hotmail.com>
2015-11-23 23:30:38 +0000

    init

---
+++ estilos/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using Estilos.Resources;
+
+namespace Estilos
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
